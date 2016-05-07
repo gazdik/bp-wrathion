50bb378c207298b757391937148771caa0a8bef0
Peter Gazdik <xgazdi03@stud.fit.vutbr.cz>
2016-05-07 15:02:48 +0200

    [general] Script for running experiments

---
+++ bin/run_experiments.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# 1:markov_bin 2:orig_bin 3: test_files_folder 4:out_file
+
+function run_markov_gpu
+{
+  bin=$1
+  test_folder=$2
+  output_file=$3
+  test_file=$4
+  devices=$5
+
+  printf "\n===========================================\
+          \nTest file: %s\nDevices: %s\
+          \n===========================================\n" \
+          $test_file $devices | tee -a $output_file
+  (time ./$bin -f $2/$test_file -S stats/rockyou.wstat -T 26 -d $devices) 2>> $output_file
+}
+
+function run_markov_cpu
+{
+  bin=$1
+  test_folder=$2
+  output_file=$3
+  test_file=$4
+  threads=$5
+
+  printf "\n===========================================\
+          \nTest file: %s\nThreads: %d\
+          \n===========================================\n" \
+          $test_file $threads | tee -a $output_file
+  (time ./$bin -f $2/$test_file -S stats/rockyou.wstat -T 26 -t $threads -c) 2>> $output_file
+}
+
+function run_brute_gpu
+{
+  bin=$1
+  test_folder=$2
+  output_file=$3
+  test_file=$4
+  devices=$5
+
+  printf "\n===========================================\
+          \nTest file: %s\nDevices: %s\
+          \n===========================================\n" \
+          $test_file $devices | tee -a $output_file
+  (time ./$bin -f $2/$test_file -d $devices) 2>> $output_file
+}
+
+function run_brute_cpu
+{
+  bin=$1
+  test_folder=$2
+  output_file=$3
+  test_file=$4
+  threads=$5
+
+  printf "\n===========================================\
+          \nTest file: %s\nThreads: %d\
+          \n===========================================\n" \
+          $test_file $threads | tee -a $output_file
+  (time ./$bin -f $2/$test_file -t $threads -c) 2>> $output_file
+}
+
+markov_bin=$1
+brute_bin=$2
+test_files_folder=$3
+out_file=$4
+
+run_markov_gpu $markov_bin $test_files_folder $out_file "r5_3.pdf" "0:0:65536"
+run_brute_gpu $brute_bin $test_files_folder $out_file "r5_3.pdf" "0:0:65536"
+run_markov_cpu $markov_bin $test_files_folder $out_file "r5_3.pdf" 4
+run_brute_cpu $brute_bin $test_files_folder $out_file "r5_3.pdf" 4
